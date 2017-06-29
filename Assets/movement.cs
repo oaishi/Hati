fc2b2fbda1cf14e6cb10115f9ffe655b46601a98
oaishi <oaishi.faria@gmail.com>
2017-06-29 23:03:18 +0600

    Hati Full
    Hati Version 1

---
+++ Assets/movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	public float moveSpeed;
+	public GameObject bunny;
+	// Use this for initialization
+	void Start () {
+		bunny.transform.localPosition = new Vector3 (0 , 3, 95 );//( 0, 0, 0 );
+		bunny.transform.localRotation = Quaternion.Euler (18, 180, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//MoveObj ();
+	}
+
+	void FixedUpdate()
+	{
+		MoveObj ();
+	}
+
+
+	void MoveObj() {		
+		float moveAmount = Time.smoothDeltaTime * moveSpeed;
+		transform.Translate ( 0f, 0f, moveAmount );	
+	}
+
+
+}
